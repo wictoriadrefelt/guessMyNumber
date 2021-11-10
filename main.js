731d32e1bcc203c0587a8bf12401e95c07dd31d3
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-11-10 15:51:08 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict'; 
+
+console.log(document.querySelector('#message').textContent);
+document.querySelector('#message').textContent = 'Correct Number!'
+console.log(document.querySelector('#message').textContent);
+
+
+
+console.log(document.querySelector('.question').textContent)
+console.log(document.querySelector('#score').textContent);
