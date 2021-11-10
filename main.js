9683450ba99c0f85bb87ca39eec922d8e78afb77
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-11-10 19:16:08 +0100

    changed layout

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 'use strict'; 
-
+/*
 console.log(document.querySelector('#message').textContent);
 document.querySelector('#message').textContent = 'Correct Number!'
 console.log(document.querySelector('#message').textContent);
 
 
 
-console.log(document.querySelector('.question').textContent)
-console.log(document.querySelector('#score').textContent);
+console.log(document.querySelector('.question').textContent = 13)
+console.log(document.querySelector('#score').textContent = 10)
+
+console.log(document.querySelector('.check-num').value)
+document.querySelector('.check-num').value = 23;
+console.log(document.querySelector('.check-num').value)
+*/
+document.querySelector('.check').addEventListener('click', function() {
+    console.log(document.querySelector('.check-num').value);
+});
